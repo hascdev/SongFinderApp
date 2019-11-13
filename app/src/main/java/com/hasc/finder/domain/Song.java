ce8a0611612a7fa0c8d88ef478eaab9ffdb8848f
Alejandro <ale.sandoval.ii@gmail.com>
2019-11-13 11:01:22 -0300

    Implementamos serializable para poder enviar a traves del Activity.

--- app/src/main/java/com/hasc/finder/domain/Song.java
+++ app/src/main/java/com/hasc/finder/domain/Song.java
@@ -2,9 +2,10 @@ package com.hasc.finder.domain;
 
 import androidx.annotation.Nullable;
 
+import java.io.Serializable;
 import java.util.Objects;
 
-public final class Song {
+public final class Song implements Serializable {
 
     private final int id;
     private final String artistName;
